c61c874cc6a69604560cd3e6306a3f7caa55d5a0
Tshidy <hlaleletshidy@gmail.com>
2024-04-24 09:18:50 +0200

    Added function that increments

--- Javascript/PassengerCounter/index.js
+++ Javascript/PassengerCounter/index.js
@@ -1,1 +1,18 @@
-﻿
+﻿//document.getElementById("count-el").innerText = 5;
+
+//let count = 0; //the variable is going to hold data
+//console.log(count);
+//Pick up here - The onclick event listener
+
+//document.addEventListener("click", increment);
+//first initialize the count
+let count = 0;
+let countElement = document.getElementById("count-el");
+let incrementBtn = document.querySelector("#increment-btn");
+
+function increment() {
+  count = count + 1;
+  countElement.innerText = count;
+}
+
+incrementBtn.addEventListener("click", increment);
