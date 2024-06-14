f2e477deb0d68c4a2f11c3be9180c622e1f78265
Tshidy <hlaleletshidy@gmail.com>
2024-06-14 22:30:49 +0200

    22 June 2024

---
+++ Javascript/Random Practice/reverseString.js
@@ -0,0 +1,25 @@
+﻿//reverse the provided string
+//convert string into array and split it split("")
+//initialize variable to empty array
+//loop through in reverse
+
+function reverseString(str) {
+  let convertedStr = str.split("");
+  console.log(convertedStr);
+  let reversedString = convertedStr.reverse();
+  let final = reversedString.join("");
+
+  return final;
+  //return str.split("").reverse().join("")
+}
+//console.log(reverseString("hello"));
+
+//Alternative way
+function reverseStr(str) {
+  let final = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    final += str[i];
+  }
+  return final;
+}
+console.log(reverseStr("lemon"));
