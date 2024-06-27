7e552f3325ea23ea348d3fe34526e42e17535371
Tshidy <hlaleletshidy@gmail.com>
2024-06-27 20:55:40 +0200

    27 June 2024

---
+++ Javascript/Random Practice/snake_case.js
@@ -0,0 +1,5 @@
+﻿function convertToSnakeCase(str) {
+  const lowerCasedStr = str.toLowerCase();
+  return lowerCasedStr.replace(/[^a-z]/g, "_");
+}
+console.log(convertToSnakeCase("The+dog>is*Chasing(the CAT"));
