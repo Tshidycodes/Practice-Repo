5d579ca0f22a70efd0299410308ea6de03650afb
Tshidy <hlaleletshidy@gmail.com>
2024-07-07 09:54:30 +0200

    practiced scoping

---
+++ Javascript/asynchronous_javascript/index.js
@@ -0,0 +1,5 @@
+﻿console.log("I");
+console.log(" eat ");
+console.log(" ice cream ");
+console.log(" with a ");
+console.log(" spoon ");
--- Javascript/var_and_let/var.js
+++ Javascript/var_and_let/var.js
@@ -1,4 +1,41 @@
-﻿console.log(myLet);
-let myLet;
-console.log(myVar);
-var myVar;
+﻿//VAR
+//When you declare a variable with the var keyword, it is declared globally, or locally if declared inside a function.
+var numArray = [];
+for (var i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray); // [0,1,2]
+console.log(i); // 3
+//After the loop exits, i retains its last value, which is 3. This is because var declares i in the function scope (or globally if not inside a function), not block scope. Therefore, i is still accessible and holds the value 3.
+
+//The above code is similar to this one: where i is declared globally
+var numArray = [];
+var i; //declared i globally
+for (i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray);
+console.log(i);
+
+var printNumTwo;
+for (var i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function () {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo()); //prints out the global value of i which is 3, instead of 2
+
+//Let keyword behaves differently
+
+let printNumTwo;
+for (let i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function () {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo()); //2
+console.log(i); // i is not defined because it was not declared in the global scope. It is only declared within the for loop statement
