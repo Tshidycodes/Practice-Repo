924e94e99df14447e8cc2d9f14e7a6240dadc57e
Tshidy <hlaleletshidy@gmail.com>
2024-07-08 08:10:25 +0200

    Free Code Camp ES6 tasks

---
+++ Javascript/const/constChallenge.js
@@ -0,0 +1,11 @@
+﻿//An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  let removed = s.pop();
+  s.unshift(removed);
+  return s;
+  // Using s = [2, 5, 7] would be invalid
+  // Only change code above this line
+}
+console.log(editInPlace());
