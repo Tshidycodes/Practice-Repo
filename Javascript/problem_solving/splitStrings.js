c6727788c17d0fe9ae746aa38f1a66ea4ed7a06e
Tshidy <hlaleletshidy@gmail.com>
2024-07-10 23:58:47 +0200

    10 July 2024

---
+++ Javascript/problem_solving/splitStrings.js
@@ -0,0 +1,13 @@
+﻿/*Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef'] */
+
+function splitString(str) {
+  let result = [];
+  for (let i = 0; i < str.length; i += 2) {
+    let pair = str.substring(i, 2);
+  }
+}
